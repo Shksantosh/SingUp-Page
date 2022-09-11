9e0dfad16b1c73e54ea10ffe1d06e00c6487a3d1
santosh kumar <santoshking82@gmail.com>
2022-09-11 22:12:45 +0530

    dux

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const { options } = require("request");
+
+const app = express();
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", function(req, res){
+
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                marge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName,
+                }
+
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https:us17.api.mailchimp.com/3.0/lists/f120af7e43";
+
+    const options = {
+        method: "POST",
+        auth: "Dux7:7c41d5e88fa61ad2ed9c641b6afb2f5e-us17"
+    }
+  
+    const request = https.request(url, options, function(response){
+
+        if ( response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+    
+
+    // request.write(jsonData);
+    request.end();
+    
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/")
+})
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server is running on port 3000")
+})
+
+// API key
+//  7c41d5e88fa61ad2ed9c641b6afb2f5e-us17
+//  Audience ID - f120af7e43
